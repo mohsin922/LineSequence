659e49a6fc08705ca68900459b8c6197ae65b420
unknown <mohsinpeer22@gmail.com>
2021-12-17 17:38:54 +0530

    Length of Line

--- Line Sequence/Program.cs
+++ Line Sequence/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Line_Sequence
 {
-    class Program
+    class program
     {
-        static void Main(string[] args)
+        public static double Length(double x1, double x2, double y1, double y2)
         {
-            Console.WriteLine("Hello World!");
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please Enter X1 and Y1 Co-Ordinates:");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter X2 and Y2 Co-Ordinates:");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+            var result = Length(x1, x2, y1, y2);
+            Console.WriteLine("Length of line between cordinates {0},{1} and {2},{3} is {4}", x1, y1, x2, y2, result);
         }
     }
+
+        
+
 }
+    
+
+
+        
