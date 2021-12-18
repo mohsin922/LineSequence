a0223933a7ed03b122d4e090f7cb80ad0f366ce0
unknown <mohsinpeer22@gmail.com>
2021-12-18 18:27:34 +0530

    Equality of Lines

--- Line Sequence/Program.cs
+++ Line Sequence/Program.cs
@@ -19,13 +19,27 @@ namespace Line_Sequence
             var y2 = Convert.ToDouble(Console.ReadLine());
             var result = Length(x1, x2, y1, y2);
             Console.WriteLine("Length of line between cordinates {0},{1} and {2},{3} is {4}", x1, y1, x2, y2, result);
+
+
+            Console.WriteLine("Please Enter X3 and Y3 Co-Ordinates:");
+            var x3 = Convert.ToDouble(Console.ReadLine());
+            var y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter X4 and Y4 Co-Ordinates:");
+            var x4 = Convert.ToDouble(Console.ReadLine());
+            var y4 = Convert.ToDouble(Console.ReadLine());
+            var result1 = Length(x3, x4, y3, y4);
+            Console.WriteLine("Length of line between cordinates {0},{1} and {2},{3} is {4}", x3, y3, x4, y4, result1);
+            if( result == result1)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
         }
     }
 
-        
+
 
 }
-    
-
-
-        
